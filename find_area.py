fa588852b9d92fe25c2047e600b710c9e4a88fdb
mahaleaditya900 <85475016+mahaleaditya900@users.noreply.github.com>
2021-06-07 12:05:35 +0530

    Add pytest

--- find_area.py
+++ find_area.py
@@ -1,6 +1,5 @@
 import math
 
+
 def find_area(r):
     return math.pi * r * r
-
-print(find_area(5))
---
+++ test_area.py
@@ -0,0 +1,5 @@
+import find_area
+
+
+def test_area():
+    assert round(find_area.find_area(5)) == 79
